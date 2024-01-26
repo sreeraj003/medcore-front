533f80d14a371541c7087005f52c2a184850a8c7
Sreeraj K <sreerajkattippallam003@gmail.com>
2024-01-26 12:23:33 +0530

    socket

--- src/context/socket/socketProvider.jsx
+++ src/context/socket/socketProvider.jsx
@@ -11,7 +11,7 @@ export const useSocket = () => {
 
 export const SocketProvider = (props) => {
 
-    const socket = useMemo(() => io('localhost:8080'), [])
+    const socket = useMemo(() => io('https://medcore.onrender.com'), [])
     return (
         <SocketContext.Provider value={socket} >
             {props.children}
